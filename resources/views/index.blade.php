a85f942680abe1df99e998dedb1eb4f7a15a3408
mariayerossi <maria_y20@mhs.istts.ac.id>
2024-04-06 13:31:18 +0700

    lalala

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,22 +42,19 @@
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="about.html">About</a>
+                        <a class="nav-link" href="#about">About</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="services.html">Services</a>
+                        <a class="nav-link" href="#gallery">Vehicles</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="gallery.html">Vehicles</a>
+                        <a class="nav-link" href="#client">Client</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="client.html">Client</a>
-                     </li>
-                     <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
+                        <a class="nav-link" href="#contact">Contact</a>
                      </li>
                   </ul>
                   <form class="form-inline my-2 my-lg-0">
@@ -71,7 +68,7 @@
          <div class="container">
             <div class="call_taital">
                <div class="call_text"><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left_15">Location</span></a></div>
-               <div class="call_text"><a href="#"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+62) 811-8000-071</span></a></div>
+               <div class="call_text"><a href="https://wa.me/+628118000071"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+62) 811-8000-071</span></a></div>
             </div>
          </div>
       </div>
@@ -88,7 +85,7 @@
                               <p class="banner_text">Kami siap memberikan pengalaman wisata yang tak terlupakan dengan rental Hiace kami, sesuai dengan kebutuhan dan keinginan Anda!</p>
                               <div class="btn_main">
                                  <div class="contact_bt"><a href="#">Booking</a></div>
-                                 <div class="contact_bt active"><a href="#">Contact Us</a></div>
+                                 <div class="contact_bt active"><a href="https://wa.me/+628118000071">Contact Us</a></div>
                               </div>
                            </div>
                         </div>
@@ -103,7 +100,7 @@
       </div>
       <!-- banner section end -->
       <!-- about section start -->
-      <div class="about_section layout_padding">
+      <div class="about_section layout_padding" id="about">
          <div class="container">
             <div class="about_section_2">
                <div class="row">
@@ -113,64 +110,15 @@
                   <div class="col-md-6"> 
                      <div class="about_taital_box">
                         <h1 class="about_taital">About <span style="color: #fe5b29;">Us</span></h1>
-                        <p class="about_text">going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined </p>
-                        <div class="readmore_btn"><a href="#">Read More</a></div>
+                        <p class="about_text">Central Hiace Rent Jatim platform penyewaan mobil yang didedikasikan untuk memenuhi kebutuhan perjalanan wisata Anda dengan kenyamanan dan keamanan yang tak tertandingi. Dengan fokus pada penyediaan layanan penyewaan mobil khususnya Toyota Hiace, kami bertekad untuk memberikan pengalaman perjalanan yang tak terlupakan bagi pelanggan kami.</p>
                      </div>
                   </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- about section end -->
-      <div class="search_section">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <h1 class="search_taital">Search Your Best Cars</h1>
-                  <p class="search_text">Using 'Content here, content here', making it look like readable</p>
-                  <!-- select box section start -->
-                  <div class="container">
-                     <div class="select_box_section">
-                        <div class="select_box_main">
-                           <div class="row">
-                              <div class="col-md-3 select-outline">
-                                 <select class="mdb-select md-form md-outline colorful-select dropdown-primary">
-                                    <option value="" disabled selected>Any Brand</option>
-                                    <option value="1">Option 1</option>
-                                    <option value="2">Option 2</option>
-                                    <option value="3">Option 3</option>
-                                 </select>
-                              </div>
-                              <div class="col-md-3 select-outline">
-                                 <select class="mdb-select md-form md-outline colorful-select dropdown-primary">
-                                    <option value="" disabled selected>Any type</option>
-                                    <option value="1">Option 1</option>
-                                    <option value="2">Option 2</option>
-                                    <option value="3">Option 3</option>
-                                 </select>
-                              </div>
-                              <div class="col-md-3 select-outline">
-                                 <select class="mdb-select md-form md-outline colorful-select dropdown-primary">
-                                    <option value="" disabled selected>Price</option>
-                                    <option value="1">Option 1</option>
-                                    <option value="2">Option 2</option>
-                                    <option value="3">Option 3</option>
-                                 </select>
-                              </div>
-                              <div class="col-md-3">
-                                 <div class="search_btn"><a href="#">Search Now</a></div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- select box section end -->
                </div>
             </div>
          </div>
       </div>
       <!-- gallery section start -->
-      <div class="gallery_section layout_padding">
+      <div class="gallery_section layout_padding" id="gallery">
          <div class="container">
             <div class="row">
                <div class="col-md-12">
@@ -184,7 +132,6 @@
                         <div class="gallery_img"><img src="images/img-1.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
@@ -192,7 +139,6 @@
                         <div class="gallery_img"><img src="images/img-2.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
@@ -200,7 +146,6 @@
                         <div class="gallery_img"><img src="images/img-3.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                </div>
@@ -212,7 +157,6 @@
                         <div class="gallery_img"><img src="images/img-1.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
@@ -220,7 +164,6 @@
                         <div class="gallery_img"><img src="images/img-2.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                   <div class="col-md-4">
@@ -228,7 +171,6 @@
                         <div class="gallery_img"><img src="images/img-3.png"></div>
                         <h3 class="types_text">Toyota car</h3>
                           <p class="looking_text">Start per day $4500</p>
-                        <div class="read_bt"><a href="#">Book Now</a></div>
                      </div>
                   </div>
                </div>
@@ -246,28 +188,29 @@
             </div>
             <div class="choose_section_2">
                <div class="row">
-                  <div class="col-sm-4">
-                     <div class="icon_1"><img src="images/icon-1.png"></div>
-                     <h4 class="safety_text">SAFETY & SECURITY</h4>
-                     <p class="ipsum_text">variations of passages of Lorem Ipsum available, but the majority have </p>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="icon_1"><img src="images/icon-2.png"></div>
-                     <h4 class="safety_text">Online Booking</h4>
-                     <p class="ipsum_text">variations of passages of Lorem Ipsum available, but the majority have </p>
-                  </div>
-                  <div class="col-sm-4">
-                     <div class="icon_1"><img src="images/icon-3.png"></div>
-                     <h4 class="safety_text">Best Drivers</h4>
-                     <p class="ipsum_text">variations of passages of Lorem Ipsum available, but the majority have </p>
-                  </div>
+                   <div class="col-sm-4">
+                       <div class="icon_1"><img src="images/icon-1.png"></div>
+                       <h4 class="safety_text">Keamanan & Keselamatan</h4>
+                       <p class="ipsum_text">Keamanan dan keselamatan adalah prioritas kami. Kami memiliki standar ketat untuk menjamin pengalaman perjalanan yang aman dan nyaman bagi pelanggan kami.</p>
+                   </div>
+                   <div class="col-sm-4">
+                       <div class="icon_1"><img src="images/icon-2.png"></div>
+                       <h4 class="safety_text">Pemesanan Online</h4>
+                       <p class="ipsum_text">Kami menawarkan layanan pemesanan online yang cepat dan mudah untuk memesan mobil Hiace, kapan saja, di mana saja.</p>
+                   </div>
+                   <div class="col-sm-4">
+                       <div class="icon_1"><img src="images/icon-3.png"></div>
+                       <h4 class="safety_text">Pengemudi Terbaik</h4>
+                       <p class="ipsum_text">Kami bekerja dengan pengemudi-pengemudi berpengalaman dan profesional yang menyediakan pengalaman berkendara yang aman dan menyenangkan.</p>
+                   </div>
                </div>
-            </div>
+           </div>
+           
          </div>
       </div>
       <!-- choose section end -->
       <!-- client section start -->
-      <div class="client_section layout_padding">
+      <div class="client_section layout_padding" id="client">
          <div class="container">
             <div id="custom_slider" class="carousel slide" data-ride="carousel">
                <div class="carousel-inner">
@@ -362,68 +305,30 @@
             </div>
          </div>
       </div>
-      <!-- client section end -->
-      <!-- contact section start -->
-      <div class="contact_section layout_padding">
-         <div class="container">
-            <div class="row">
-               <div class="col-sm-12">
-                  <h1 class="contact_taital">Get In Touch</h1>
-               </div>
-            </div>
-         </div>
-         <div class="container">
-            <div class="contact_section_2">
-               <div class="row">
-                  <div class="col-md-12">
-                     <div class="mail_section_1">
-                        <input type="text" class="mail_text" placeholder="Name" name="Name">
-                        <input type="text" class="mail_text" placeholder="Email" name="Email">
-                        <input type="text" class="mail_text" placeholder="Phone Number" name="Phone Number">
-                        <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
-                        <div class="send_bt"><a href="#">Send</a></div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- contact section end -->
-      <!-- footer section start -->
-      <div class="footer_section layout_padding">
+
+      <div class="footer_section layout_padding" id="contact">
          <div class="container">
             <div class="row">
                <div class="col-md-12">
-                  <div class="footeer_logo"><img src="images/logo.png"></div>
+                  <div class="footeer_logo"><a href="/">
+                     <h1 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;color:white">Central Hiace Rent Jatim</h1>
+                 </a></div>
                </div>
             </div>
             <div class="footer_section_2">
                <div class="row">
                   <div class="col">
-                     <h4 class="footer_taital">Subscribe Now</h4>
-                     <p class="footer_text">There are many variations of passages of Lorem Ipsum available,</p>
-                     <div class="form-group">
-                        <textarea class="update_mail" placeholder="Enter Your Email" rows="5" id="comment" name="Enter Your Email"></textarea>
-                        <div class="subscribe_bt"><a href="#">Subscribe</a></div>
-                     </div>
-                  </div>
-                  <div class="col">
                      <h4 class="footer_taital">Information</h4>
-                     <p class="lorem_text">There are many variations of passages of Lorem Ipsum available, but the majority </p>
-                  </div>
-                  <div class="col">
-                     <h4 class="footer_taital">Helpful Links</h4>
-                     <p class="lorem_text">There are many variations of passages of Lorem Ipsum available, but the majority </p>
-                  </div>
-                  <div class="col">
-                     <h4 class="footer_taital">Invesments</h4>
-                     <p class="lorem_text">There are many variations of passages of Lorem Ipsum available, but the majority </p>
+                     <div class="location_text"><a href="/"><span class="padding_left_15">Home</span></a></div>
+                     <div class="location_text"><a href="#about"><span class="padding_left_15">About</span></a></div>
+                     <div class="location_text"><a href="#gallery"><span class="padding_left_15">Vehicles</span></a></div>
+                     <div class="location_text"><a href="#client"><span class="padding_left_15">Client</span></a></div>
                   </div>
                   <div class="col">
                      <h4 class="footer_taital">Contact Us</h4>
                      <div class="location_text"><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left_15">Location</span></a></div>
-                     <div class="location_text"><a href="#"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+71) 8522369417</span></a></div>
-                     <div class="location_text"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left_15">demo@gmail.com</span></a></div>
+                     <div class="location_text"><a href="https://wa.me/+628118000071"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+62) 811-8000-071</span></a></div>
+                     <div class="location_text"><a href="mailto:someone@example.com"><i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left_15">demo@gmail.com</span></a></div>
                      <div class="social_icon">
                         <ul>
                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
@@ -443,7 +348,7 @@
          <div class="container">
             <div class="row">
                <div class="col-sm-12">
-                  <p class="copyright_text">2023 All Rights Reserved. Design by <a href="https://html.design">Free Html Templates</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a></p>
+                  <p class="copyright_text">2024 All Rights Reserved</a></p>
                </div>
             </div>
          </div>
