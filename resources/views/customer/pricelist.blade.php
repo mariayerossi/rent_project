f485f1da469efcf22f589190cd7a3763e5d573bf
mariayerossi <maria_y20@mhs.istts.ac.id>
2024-04-06 17:52:36 +0700

    yezz

--- resources/views/customer/pricelist.blade.php
+++ resources/views/customer/pricelist.blade.php
@@ -1,6 +1,146 @@
-{{-- @include('customer.layout') --}}
-@extends('customer.layout')
+@extends('layouts.customer')
 
 @section('content')
-<h1>halo</h1>
+<style>
+    .custom-list {
+  list-style: none; /* Menghapus default bullet */
+}
+.custom-list li:before {
+  content: "\2713"; /* Unicode untuk centang */
+  display: inline-block;
+  width: 1em; /* Menyesuaikan ukuran */
+  margin-left: -1em; /* Menggeser ke kiri agar terlihat rapi */
+}
+.custom-list2 {
+  list-style: none; /* Menghapus default bullet */
+}
+.custom-list2 li:before {
+  content: "\2717"; /* Unicode untuk silang */
+  display: inline-block;
+  width: 1em; /* Menyesuaikan ukuran */
+  margin-left: -1em; /* Menggeser ke kiri agar terlihat rapi */
+}
+ul li {
+    font-size: 20px;
+}
+.card-text {
+    font-size: 20px;
+}
+</style>
+<div class="container mb-5">
+    <h1 class="text-center"><b>Pricelist</b></h1>
+    <div class="row mt-3">
+        <div class="col-md-3">
+            <div class="card text-center" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+                <div class="card-header bg-primary text-white">
+                    <h3 class="text-white">City Tour</h3>
+                    <h4> </h4>
+                </div>
+                <div class="card-body d-flex flex-column" style="height: 520px;">
+                    <h2 class="card-title">Rp 1.100.000/12 jam</h2>
+                    <p class="card-text">
+                        <h3>Sudah Termasuk:</h3>
+                        <ul class="custom-list mb-3">
+                            <li>Mobil</li>
+                            <li>Driver</li>
+                            <li>BBM</li>
+                        </ul>
+                        <h3>Tidak Termasuk:</h3>
+                        <ul class="custom-list2">
+                            <li>Biaya Parkir</li>
+                            <li>Biaya Tol</li>
+                        </ul>
+                    </p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn btn-primary">Booking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-center" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+                <div class="card-header bg-success text-white">
+                    <h3 class="text-white">Zona I</h3>
+                    <h4 class="text-white">(Jawa Timur ≥ 175 km)</h4>
+                </div>
+                <div class="card-body d-flex flex-column" style="height: 500px;">
+                    <h2 class="card-title">Rp 1.300.000/12 jam</h2>
+                    <h4 class="card-title">(max. 15 jam)</h4>
+                    <p class="card-text">
+                        <h3>Sudah Termasuk:</h3>
+                        <ul class="custom-list mb-3">
+                            <li>Mobil</li>
+                            <li>Driver</li>
+                        </ul>
+                        <h3>Tidak Termasuk:</h3>
+                        <ul class="custom-list2">
+                            <li>BBM</li>
+                            <li>Biaya Parkir</li>
+                            <li>Biaya Tol</li>
+                        </ul>
+                    </p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn btn-primary">Booking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-center" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+                <div class="card-header bg-secondary text-white">
+                    <h3 class="text-white">Zona II</h3>
+                    <h4 class="text-white">(Jawa Tengah ≥ 400 km)</h4>
+                </div>
+                <div class="card-body d-flex flex-column" style="height: 500px;">
+                    <h2 class="card-title">Rp 1.500.000/hari</h2>
+                    <h4 class="card-title">(min. 2 hari)</h4>
+                    <p class="card-text">
+                        <h3>Sudah Termasuk:</h3>
+                        <ul class="custom-list mb-3">
+                            <li>Mobil</li>
+                            <li>Driver</li>
+                        </ul>
+                        <h3>Tidak Termasuk:</h3>
+                        <ul class="custom-list2">
+                            <li>BBM</li>
+                            <li>Biaya Parkir</li>
+                            <li>Biaya Tol</li>
+                        </ul>
+                    </p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn btn-primary">Booking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-center" style="box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);">
+                <div class="card-header bg-danger text-white">
+                    <h3 class="text-white">Zona III</h3>
+                    <h4 class="text-white">(Jawa Barat ≥ 800 km)</h4>
+                </div>
+                <div class="card-body d-flex flex-column" style="height: 500px;">
+                    <h2 class="card-title">Rp 1.700.000/hari</h2>
+                    <h4 class="card-title">(min. 3 hari)</h4>
+                    <p class="card-text">
+                        <h3>Sudah Termasuk:</h3>
+                        <ul class="custom-list mb-3">
+                            <li>Mobil</li>
+                            <li>Driver</li>
+                        </ul>
+                        <h3>Tidak Termasuk:</h3>
+                        <ul class="custom-list2">
+                            <li>BBM</li>
+                            <li>Biaya Parkir</li>
+                            <li>Biaya Tol</li>
+                        </ul>
+                    </p>
+                    <div class="mt-auto">
+                        <a href="#" class="btn btn-primary">Booking</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -36,14 +36,11 @@
                <a href="/" class="logo d-flex align-items-center">
                    <h1 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;color:white">Central Hiace Rent Jatim</h1>
                </a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              </button>
            </nav>
         </div>
      </div>
      <!-- header section end -->
-     <div class="call_text_main">
+     <div class="call_text_main mb-4">
         <div class="container">
            <div class="call_taital">
               <div class="call_text"><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left_15">Location</span></a></div>
@@ -51,7 +48,56 @@
            </div>
         </div>
      </div>
+
       @yield('content')
+      
+      <div class="footer_section layout_padding" id="contact">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="footeer_logo"><a href="/">
+                     <h1 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;color:white">Central Hiace Rent Jatim</h1>
+                 </a></div>
+               </div>
+            </div>
+            <div class="footer_section_2">
+               <div class="row">
+                  <div class="col">
+                     <h4 class="footer_taital">Information</h4>
+                     <div class="location_text"><a href="/"><span class="padding_left_15">Home</span></a></div>
+                     <div class="location_text"><a href="/#about"><span class="padding_left_15">About</span></a></div>
+                     <div class="location_text"><a href="/#gallery"><span class="padding_left_15">Vehicles</span></a></div>
+                     <div class="location_text"><a href="/#client"><span class="padding_left_15">Client</span></a></div>
+                  </div>
+                  <div class="col">
+                     <h4 class="footer_taital">Contact Us</h4>
+                     <div class="location_text"><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span class="padding_left_15">Location</span></a></div>
+                     <div class="location_text"><a href="https://wa.me/+628118000071"><i class="fa fa-phone" aria-hidden="true"></i><span class="padding_left_15">(+62) 811-8000-071</span></a></div>
+                     <div class="location_text"><a href="mailto:someone@example.com"><i class="fa fa-envelope" aria-hidden="true"></i><span class="padding_left_15">demo@gmail.com</span></a></div>
+                     <div class="social_icon">
+                        <ul>
+                           <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                           <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                           <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                           <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <p class="copyright_text">2024 All Rights Reserved</a></p>
+               </div>
+            </div>
+         </div>
+      </div>
       <script src="{{asset('js/jquery.min.js')}}"></script>
       <script src="{{asset('js/popper.min.js')}}"></script>
       <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
